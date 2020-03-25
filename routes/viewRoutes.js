6456ca382c6326730d44c1696c599073456c5de2
jimtie <jimmychen.xin@gmail.com>
2020-03-25 12:39:43 -0700

    first page connected

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // res.sendFile('views/index.html', {
-    //     root: __dirname + '/../',
-    // });
+    res.sendFile('public/views/index.html', {
+        root: __dirname + '/../',
+    });
 
-    res.send('home page goes here');
 
 });
 
