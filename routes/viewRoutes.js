efd44577da3a039e3b1afb5a27d7954e28baf474
anya-pich <anna.pich@outlook.com>
2020-03-25 12:25:51 -0700

    added controllers

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // res.sendFile('views/index.html', {
-    //     root: __dirname + '/../',
-    // });
 
-    res.send('home page goes here');
+    res.sendFile('/public/views/index.html', {
+        root: __dirname + '/../',
+    });
+
+});
+
+router.get('/login', (req, res) => {
+
+    res.sendFile('/public/views/signup_login/login.html', {
+        root: __dirname + '/../',
+    });
+
+});
+
+router.get('/signup', (req, res) => {
+
+    res.sendFile('/public/views/signup_login/signup.html', {
+        root: __dirname + '/../',
+    });
 
 });
 
